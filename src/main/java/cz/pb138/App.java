85a44f7132683b13674db816f7b729ee19d8d13d
krylias <trubelik@gamesia.cz>
2018-04-18 01:32:00 +0200

    Begin
    Základní struktura a nastavení projektu pro vývoj v IDEA.

---
+++ src/main/java/cz/pb138/App.java
@@ -0,0 +1,28 @@
+package cz.pb138;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+
+public class App {
+    private JPanel panelMain;
+
+    public App() {
+        panelMain.addContainerListener(new ContainerAdapter() {
+            @Override
+            public void componentAdded(ContainerEvent e) {
+                super.componentAdded(e);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
